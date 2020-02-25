72cbc797084da87989a715e34797f2f9abf24292
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2020-02-25 15:04:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+#PHP Clipboard
+
+This is a 5-minute package I threw togehter that simply wraps the Mac, Windows, and (most common) Linux commands for copying contents to the clipboard. I don't handle reading files. You'll have to do that yourself. I simply pipe whatever contents you provide to the appropriate command based on the operating system reported by php_uname().
+
+### Installation
+```shell script
+composer require edgrosvenor/php-clipboard
+```
+
+### Usage
+```php
+<?php
+
+use Edgrosvenor\PHPClipboard\Clipboard;
+
+class CopyStuff 
+{
+    public function copyMyName($name = 'Ed Grosvenor')
+    {
+        $clipboard = new Clipboard($name);
+        $clipboard->copy();
+        // 'Ed Grosvenor' has been copied to your clipboard
+    }
+}
+```
---
+++ src/Clipboard.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Edgrosvenor\PHPClipboard;
+
+
+class Clipboard
+{
+
+    public $contents;
+    private $os;
+
+    public function __construct($contents)
+    {
+        $this->contents = $contents;
+        $this->os = php_uname();
+    }
+
+    public function copy(): void
+    {
+
+        if (preg_match('/windows/i', $this->os)) {
+            $this->windows();
+            return;
+        }
+        if (preg_match('/darwin/i', $this->os)) {
+            $this->mac();
+            return;
+        }
+        $this->linux();
+    }
+
+    private function mac(): void
+    {
+        exec('echo ' . $this->contents . ' | pbcopy');
+    }
+
+    private function linux(): void
+    {
+        exec('echo ' . $this->contents . ' | xclip -sel clip');
+    }
+
+    private function windows(): void
+    {
+        exec('echo ' . $this->contents . ' | clip');
+    }
+}
